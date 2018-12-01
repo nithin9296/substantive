b629a80921ebebe09d5b7cc23346fb618549ef4f
nithin9296 <nithin9196@gmail.com>
2018-12-01 06:29:49 +0400

    Update requirements.txt

--- substantive/settings/local.py
+++ substantive/settings/local.py
@@ -35,7 +35,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 DEFAULT_FROM_EMAIL = 'shrotaapp@gmail.com'
 EMAIL_HOST_USER = 'shrotaapp@gmail.com'
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_PASSWORD = 'motorokr240377'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 
