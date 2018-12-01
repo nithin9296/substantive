514e0ce567a2ec8b11db477785335a1154dfc86d
nithin9296 <nithin9196@gmail.com>
2018-12-01 06:14:27 +0400

    Update requirements.txt

--- substantive/settings/production.py
+++ substantive/settings/production.py
@@ -99,10 +99,10 @@ WSGI_APPLICATION = 'substantive.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 db_from_env = dj_database_url.config()
